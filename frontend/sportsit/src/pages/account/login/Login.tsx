c1401c447f80291c321aec8a4977809e2f2f0d6b
신 우현 <woo_hyun_1@naver.com>
2023-03-13 17:40:24 +0900

    [refactor] Refactor login form by react-hook-form

--- frontend/sportsit/src/pages/account/login/Login.tsx
+++ frontend/sportsit/src/pages/account/login/Login.tsx
@@ -1,17 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
-import styled from "styled-components";
 import * as S from "./LoginStyles";
 
 const Login = () => {
+  const { register, watch, handleSubmit, formState } = useForm();
+
+  const onValid = () => {
+    console.log("Valid !");
+  };
+
+  const onInvalid = () => {
+    if (formState.errors.id) {
+      alert(`${formState.errors?.id?.message}`);
+    }
+    if (formState.errors.password) {
+      alert(`${formState.errors?.password?.message}`);
+    }
+  };
+
+  // console.log(watch());
+  // console.log(formState.errors);
+
   return (
     <S.LoginContainer>
       <S.ImageArea>
         <S.LogoImage src={require("../../../imgs/logo/AppLogo.png")} />
       </S.ImageArea>
-      <S.Form>
-        <S.Input placeholder="아이디(이메일)"></S.Input>
-        <S.Input placeholder="비밀번호"></S.Input>
+      <S.Form onSubmit={handleSubmit(onValid, onInvalid)}>
+        <S.Input
+          {...register("id", { required: "아이디는 필수 입력사항 입니다." })}
+          placeholder="아이디(이메일)"
+        ></S.Input>
+        <S.Input
+          {...register("password", {
+            required: "비밀번호는 필수 입력사항 입니다.",
+          })}
+          type="password"
+          placeholder="비밀번호"
+        ></S.Input>
         <S.SubmitButton>로그인</S.SubmitButton>
       </S.Form>
       <S.AccountPanel>
